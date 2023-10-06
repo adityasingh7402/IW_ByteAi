19f72b028f3ef191bf37151826cd9d1846886ce7
Aditya Kumar <adityasingh7402@gmail.com>
2023-10-06 15:47:06 +0530

    insia

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <div className="main h-screen relative w-full">
+        <div className="name absolute top-8 left-10 text-4xl antialiased"><div class="font-bold animate-charcter">ByteAi</div></div>
+        <div className="heading w-3/4 mx-auto h-full flex justify-center items-center flex-col">
+          <div className="name pb-10 text-6xl antialiased">Driver Sleepness Detection </div>
+          <div className="disc text-xl pb-10 text-center antialiased">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, perspiciatis. Culpa sint maiores corporis sapiente! Quis sapiente qui eum minus similique! Architecto sequi inventore doloremque numquam iste corporis quidem officiis totam pariatur esse dolor vitae ut neque nihil, quod incidunt enim dignissimos illum maiores? Autem magni quia voluptatibus quo maxime</div>
+          <div className="btn"><Link href={"./main"}><button className="glow-on-hover" type="button"><p className='text-xl antialiased uppercase font-bold'>Let's Safe</p></button></Link></div>
+        </div>
+        <div className="github absolute text-5xl bottom-8 left-10"><Link href={"https://github.com/Byte-Brains-Ai/IW_ByteAi"} target="_blank"><AiFillGithub/></Link></div>
+        <div className="github absolute text-5xl bottom-8 right-10"><Link href={"https://github.com/Byte-Brains-Ai/IW_ByteAi"} target="_blank"><AiOutlineInstagram/></Link></div>
+      </div>
+    </>
+  )
+}
---
+++ pages/main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+            <div className="mains relative h-screen overflow-hidden">
+                <div className="car absolute top-0 left-0 right-0">
+                    <img src="car.png" alt="" />
+                </div>
+                <div className="road absolute -top-52 left-0 right-0">
+                    <img src="road.jpg" alt="" />
+                </div>
+                <div className="text ">
+                    <p>helloo</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main
